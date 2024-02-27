e067010d9b96bdbefb8d7ea1f5f3185f746c481a
Paula Cano <paulacanoroman@gmail.com>
2024-02-27 21:14:51 +0100

    feat: api consumption and show in console

--- pages/components/imageList/ImageList.tsx
+++ pages/components/imageList/ImageList.tsx
@@ -16,11 +16,19 @@ const ImageList = () => {
     const [allImages, setAllImages] = useState<imagePrototype>() //GUARDA LISTA QUE RECIBE DE LA API
     const [userNumber, setUserNumber] = useState<number>() //GUARDA NUMERO QUE HA ESCRITO EL USUARIO
 
+    const apiCall = async (numImages:number)=>{
+        try{
+            const url = await fetch(`https://picsum.photos/v2/list?page=2&limit=${numImages}`);
+            const data = await url.json();
+            console.log(data)
+        }catch(error:any){console.log(error,"error")}
+    }
+
   return (
     <div>
     <h3>¿Cuántas imágenes deseas mostrar?</h3>
     <input type='number' max={50} min={0} onChange={(e)=>setUserNumber(parseInt(e.target.value))} value={userNumber}></input>
-    <button>mostrar</button>
+    <button onClick={()=>{apiCall(3)}}>mostrar</button>
     {userNumber}
     </div>
   )
