d4f5dce0f340276c86fb7e9e4c794fd3b1351e8d
Paula Cano <paulacanoroman@gmail.com>
2024-02-27 12:27:14 +0100

    feat: useState and type ts

--- pages/components/tasklist/Tasklist.tsx
+++ pages/components/tasklist/Tasklist.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tasklist = () => {
+
+    type taskPrototype = {
+        id: number,
+        text: string,
+        done: true
+    }
+
+    const [taskList, setTaskList] = useState<taskPrototype>()
+
   return (
     <div>Tasklist</div>
   )
