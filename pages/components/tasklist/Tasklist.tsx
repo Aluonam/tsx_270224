895ac854d4d6f5a5839f51481f57c1a188ca8532
Paula Cano <paulacanoroman@gmail.com>
2024-02-27 12:41:50 +0100

    feat: eskeleton button and titles

--- pages/components/tasklist/Tasklist.tsx
+++ pages/components/tasklist/Tasklist.tsx
@@ -18,10 +18,14 @@ const Tasklist = () => {
     const [inputText, setInputText] = useState<string>("ejemplo de tarea")
 
   return (
-    <div>Tasklist
-
+    <div>
+    <h2>Tasklist example with TS and React</h2>
+    <h3>Escribe una tarea</h3>
     <input type="text" onChange={(e)=>setInputText(e.target.value)} value={inputText}></input>
-
+    <button>send</button>
+    <br></br>
+    <h3>Tareas:</h3>
+    {taskList.text}
     </div>
   )
 }
