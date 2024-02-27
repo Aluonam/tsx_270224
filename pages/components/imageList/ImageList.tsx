7a03bda8ee20f365cfade86d30155729cd10028b
Paula Cano <paulacanoroman@gmail.com>
2024-02-27 21:07:04 +0100

    feat: input save the number
    with parseInt for transforme string into a number and limit the input

--- pages/components/imageList/ImageList.tsx
+++ pages/components/imageList/ImageList.tsx
@@ -13,13 +13,15 @@ const ImageList = () => {
         }
     ]
 
-    const [allImages, setAllImages] = useState<imagePrototype>()
+    const [allImages, setAllImages] = useState<imagePrototype>() //GUARDA LISTA QUE RECIBE DE LA API
+    const [userNumber, setUserNumber] = useState<number>() //GUARDA NUMERO QUE HA ESCRITO EL USUARIO
 
   return (
     <div>
     <h3>¿Cuántas imágenes deseas mostrar?</h3>
-    <input type='number' max={50}></input>
+    <input type='number' max={50} min={0} onChange={(e)=>setUserNumber(parseInt(e.target.value))} value={userNumber}></input>
     <button>mostrar</button>
+    {userNumber}
     </div>
   )
 }
