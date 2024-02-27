5a819793cfd21b260e6f2bc1970767844fb3f521
Paula Cano <paulacanoroman@gmail.com>
2024-02-27 13:02:37 +0100

    feat: add task function

--- pages/components/tasklist/Tasklist.tsx
+++ pages/components/tasklist/Tasklist.tsx
@@ -1,33 +1,41 @@
 import React, { useState } from 'react'
 
-const Tasklist = () => {
-
-    type taskPrototype = {
+  type taskPrototype = {
         id: number,
         text: string,
         done: true
     }
 
-    const [taskList, setTaskList] = useState<taskPrototype>(        
-        {
+export const Tasklist = () => {
+
+    const [listOfTask, setListOfTask] = useState<taskPrototype[]>(        
+       [ {
         id: 0,
         text: "tarea por defecto",
         done: true
-        }
+        }]
     )
     const [inputText, setInputText] = useState<string>("ejemplo de tarea")
+
+    const handleAddTask = ()=>{
+        setListOfTask([...listOfTask,{
+            id: 0,
+            text: inputText,
+            done: true
+        }])
+        console.log(listOfTask)
+        setInputText("")
+    }
 
   return (
     <div>
     <h2>Tasklist example with TS and React</h2>
     <h3>Escribe una tarea</h3>
     <input type="text" onChange={(e)=>setInputText(e.target.value)} value={inputText}></input>
-    <button>send</button>
+    <button onClick={()=>{handleAddTask()}}>send</button>
     <br></br>
     <h3>Tareas:</h3>
-    {taskList.text}
     </div>
   )
 }
 
-export default Tasklist
